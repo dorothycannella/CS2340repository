d6c4d58704612b9b587bf5d27a25089e6bfa9ba8
Raymond Ortiz <rortiz9@gatech.edu>
2015-10-05 23:10:52 -0400

    Moved code out of Game int Round & Map

---
+++ src/model/Mule.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Mule {
+    private int type;
+
+    public Mule(int type) {
+        this.type = type;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
+
